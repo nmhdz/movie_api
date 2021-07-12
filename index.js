59612e94a1cab8b56a60d2478d82dad39d6bb0bd
nmhdz <nmontana.cf@gmail.com>
2021-07-12 15:09:06 -0600

    Update index.js
    1). require express at top of file. 2). create an express GET route located at the endpoint "/movies" that returns a json object containing data of top ten movies. 3). create another GET route located at endpoint "/" that returns "Welcome to my movie API!". 4).  Utilize express.static to serve documentation file from public folder. 5). require morgan and use morgan middleware library to log requests using "common". 6). implement an error handling middleware function that will log all application-errors to terminal.

--- index.js
+++ index.js
@@ -1,2 +1,84 @@
 const express = require('express');
+  morgan = require('morgan');
+
 const app = express();
+
+//Top 10 movies
+const topMovies = [
+  {
+    title: "This Is Where I Leave You",
+    director: "Shawn Levy",
+    genre: "Drama/Comedy"
+  },
+  {
+    title: "The Judge",
+    director: "David Dobkin",
+    genre: "Drama/Comedy"
+  },
+  {
+    title: "Riding in Cars With Boys",
+    director: "Penny Marshall",
+    genre: "Drama/Comedy"
+  },
+  {
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    genre: "Crime/Comedy"
+  },
+  {
+    title: "Tempation: Confessions of a Marriage Counselor",
+    director: "Tyler Perry",
+    genre: "Drama/Romance"
+  },
+  {
+    title: "Just Married",
+    director: "Shawn Levy",
+    genre: "Romance"
+  },
+  {
+    title: "Pineapple Express",
+    director: "David Gordon Green",
+    genre: "Comedy/Action"
+  },
+  {
+    title: "The Count of Monte Cristo",
+    director: "Kevin Reynolds",
+    genre: "Adventure/Romance"
+  },
+  {
+    title: "Step Brothers",
+    director: "Adam McKay",
+    genre: "Comedy"
+  },
+  {
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    genre: "Drama/Crime"
+  }
+];
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie API!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __movie_api });
+});
+
+// error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Oops, something broke!');
+});
+
+//listen on port 8080
+app.listen(8080, () => {
+  console.log('My app is listening on port 8080.');
+});
