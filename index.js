7d62fe54a528ce00fbef3ccc53aaa2b45cfd5d51
nmhdz <nmontana.cf@gmail.com>
2021-07-20 22:45:14 -0600

    Define and document endpoints for REST API
    Create the Express code to route all of the endpoints

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 const express = require('express');
-  morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const morgan = require('morgan');
 
 const app = express();
 
+//Log to terminal
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+//Routes request for static file to public folder
+app.use(express.static('public'));
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', {
+    root: __dirname
+  });
+});
+
 //Top 10 movies
-const topMovies = [
+const movies = [
   {
     title: "This Is Where I Leave You",
     director: "Shawn Levy",
-    genre: "Drama/Comedy"
+    genre: "Drama"
   },
   {
     title: "The Judge",
     director: "David Dobkin",
-    genre: "Drama/Comedy"
+    genre: "Drama"
   },
   {
     title: "Riding in Cars With Boys",
     director: "Penny Marshall",
-    genre: "Drama/Comedy"
+    genre: "Drama"
   },
   {
     title: "Pulp Fiction",
     director: "Quentin Tarantino",
-    genre: "Crime/Comedy"
+    genre: "Crime"
   },
   {
     title: "Tempation: Confessions of a Marriage Counselor",
     director: "Tyler Perry",
-    genre: "Drama/Romance"
+    genre: "Drama"
   },
   {
     title: "Just Married",
@@ -38,12 +52,12 @@ const topMovies = [
   {
     title: "Pineapple Express",
     director: "David Gordon Green",
-    genre: "Comedy/Action"
+    genre: "Comedy"
   },
   {
     title: "The Count of Monte Cristo",
     director: "Kevin Reynolds",
-    genre: "Adventure/Romance"
+    genre: "Adventure"
   },
   {
     title: "Step Brothers",
@@ -53,32 +67,79 @@ const topMovies = [
   {
     title: "The Shawshank Redemption",
     director: "Frank Darabont",
-    genre: "Drama/Crime"
+    genre: "Drama"
   }
 ];
 
-app.use(express.static('public'));
-app.use(morgan('common'));
-
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
-
+//Returns a Welcome message at the endpoint "/"
 app.get('/', (req, res) => {
   res.send('Welcome to my movie API!');
 });
 
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname });
+//Returns data about ALL movies
+app.get('/movies', (req, res) => {
+  res.json(movies);
 });
 
-// error handling
+//Returns data about a movie by title
+app.get('/movies/:title', (req, res) => {
+  res.json(movies.find((movie) => {
+    return movie.title === req.params.title
+  }));
+});
+
+//Returns data about a genre by name
+app.get('/movies/genres/:name', (req, res) => {
+  res.send('Successful GET request returning data about a genre by name');
+});
+
+//Returns data about a director by name
+app.get('/movies/directors/:name', (req, res) => {
+  res.send('Successful GET request returning data about a director by name');
+});
+
+//Allows new users to register
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.username) {
+    const message = 'Missing username in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+//Allow users to update user info
+app.put('/users/:username', (req, res) => {
+  res.send('Successful PUT request allowing users to update user informations');
+});
+
+//Allow users to add a movie to their list of favorites
+app.post('/users/:username/movies/:movieID', (req, res) => {
+  res.send('Successful POST request allowing users to add a movie to favorites list');
+});
+
+//Allow users to remove a movie from their list of favorites
+app.delete('/users/:username/movies/:movieID', (req, res) => {
+  res.send('Successful DELETE request allowing users to remove a movie from their list of favorites');
+});
+
+//Allow existing users to deregister
+app.delete('/users/:username', (req, res) => {
+  res.send('Successful DELETE request allowing users to deregister');
+});
+
+
+//Error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Oops, something broke!');
 });
 
-//listen on port 8080
+//Listen on port 8080
 app.listen(8080, () => {
   console.log('My app is listening on port 8080.');
 });
