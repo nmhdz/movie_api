e394948478a7ba9adf77eeaac2736a3f42f541ad
nmhdz <nmontana.cf@gmail.com>
2021-08-09 22:06:48 -0600

    Update models.js
    1). Create the models for your “Movies” schema and “Users” schema using Mongoose.
    2). Export these models from “models.js” file using module.exports.
    3). Import “models.js” file into main “index.js” file, integrating the models with the rest of application.

--- models.js
+++ models.js
@@ -1,1 +1,32 @@
 const mongoose = require('mongoose');
+
+let movieShema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Description: String
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
+
+let usersSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectID, ref: 'Movie'
+}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('user', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
